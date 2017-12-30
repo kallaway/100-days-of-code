668870ab7e88309a9f4cec582017f20b036a737c
JosephWeaver <joe.w.weaver@gmail.com>
2017-12-29 23:14:04 -0800

    Moved freeCodeCamp to its own repository.

--- freeCodeCamp/Algorithms/01-reverse-string.js
+++
@@ -1,6 +0,0 @@
-// https://www.freecodecamp.org/challenges/reverse-a-string
-
-function reverseString(str) {
-  return str.split("").reverse().join("");
-}
-reverseString("hello"); // "elloh"
--- freeCodeCamp/Algorithms/03-check-for-palindromes.js
+++
@@ -1,7 +0,0 @@
-// https://www.freecodecamp.org/challenges/check-for-palindromes
-
-function palindrome(str) {
-  str = str.replace(/[\W_]/g,"").toLowerCase();
-  return str == str.split("").reverse().join("");
-}
-palindrome("Was it Eliot's toilet I saw?"); // true
