a6e0b699c9321a8659e3ded408436340a187c6d5
jain_nehil <jain.nehil@gmail.com>
2018-01-18 00:44:01 -0500

    finished day 1

--- code/brain/brain.py
+++ code/brain/brain.py
@@ -1,31 +1,43 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 import configparser
 from github import Github
-import simplejson as json
-
+import base64
+from datetime import date
 
 app = Flask(__name__)
 config = configparser.ConfigParser()
 config.read('config.ini')
 git = Github(config['GITHUB']['User'], config['GITHUB']['Password'])
 
+
+@app.errorhandler(400)
+def not_found(error):
+  return make_response(jsonify({'error': 'Bad request'}), 400)
+
+
+@app.errorhandler(404)
+def not_found(error):
+  return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 @app.route('/')
-def hello_world():
-  return 'Hello World version2!'
+def root():
+  return jsonify({'message': 'This is brain\'s internal server api'})
 
 
-@app.route('/brain/api/v1.0/logs')
+def has_todays_update(logs):
+  assert logs
+  today_str = date.today().strftime('%b %d, %Y')
+  return today_str in logs
+
+
+@app.route('/brain/api/v1.0/logs/is_updated_today', methods=['GET'])
 def get_logs():
-  for repo in git.get_user().get_repos():
-    if repo.name in '100-days-of-code':
-      for i in repo.get_commits():
-        if i.author.name == config['GITHUB']['DisplayName']:
-          for f in i.files:
-            if 'log.md' == f.filename:
-              print(f.raw_url)
-
-  return 'See Logs'
+  repo = git.get_user().get_repo('100-days-of-code')
+  progress_logs_file = repo.get_file_contents('log.md')
+  progress_logs = str(base64.standard_b64decode(progress_logs_file.content).decode('utf-8'))
+  return jsonify({'result': has_todays_update(progress_logs)})
 
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run()
--- log.md
+++ log.md
@@ -2,9 +2,14 @@
 
 ### Day 0: Jan 16, 2018
 
-**Today's Progress**: Started setting up the lambda server for brain of my bot which will automate mundane things like posting 100 days challenge social updates.
+**Today's Progress**:
 
-**Thoughts:** Setup Python3 env, Zappa and basic Flask App. Feels like it took 1.5hours to just setup and play with simple github API, need to improve my speed.
+- Started setting up the lambda server for brain of my bot.
+- Created an endpoint to return true or false if progress log.md (this file) was updated with today's progress.
 
-**Link to work:** Coming soon
+**Thoughts:** Setup Python3 env, Zappa and basic Flask App. Feels like it took 1.5 hrs to just setup and play with simple github API, need to improve my speed. Refactored logic and simplified requirements in second pass
+
+**Link to work:**
+
+- [API endpoint:=> /brain/api/v1.0/logs/is_updated_today] (https://6kvjel14vk.execute-api.us-east-1.amazonaws.com/dev/brain/api/v1.0/logs/is_updated_today)
 
