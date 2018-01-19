835c7ef97fa8d585a5635510be920cac2a651382
jain_nehil <jain.nehil@gmail.com>
2018-01-19 00:20:17 -0500

    conntectd bot with webhook on lambda

--- code/brain/brain.py
+++ code/brain/brain.py
@@ -1,13 +1,51 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 import configparser
 from github import Github
 import base64
 from datetime import date
+from fbmq import Page
+import logging
 
 app = Flask(__name__)
 config = configparser.ConfigParser()
 config.read('config.ini')
-git = Github(config['GITHUB']['User'], config['GITHUB']['Password'])
+git = Github(config['GITHUB']['USER'], config['GITHUB']['PASSWORD'])
+page = Page(config['FACEBOOK']['PAGE_TOKEN'])
+
+
+@app.route('/fb/webhook', methods=['GET'])
+def validate():
+  if request.args.get('hub.mode', '') == 'subscribe' and \
+                  request.args.get('hub.verify_token', '') == config['FACEBOOK']['VERIFY_TOKEN']:
+
+    logging.debug("Validating webhook")
+
+    return request.args.get('hub.challenge', '')
+  else:
+    return 'Failed validation. Make sure the validation tokens match.'
+
+@app.route('/fb/webhook', methods=['POST'])
+def webhook():
+  payload = request.get_data(as_text=True)
+  logging.debug("webhook payload: {}".format(payload))
+  page.handle_webhook(payload)
+
+  return "ok"
+
+
+@page.handle_message
+def message_handler(event):
+  """:type event: fbmq.Event"""
+  sender_id = event.sender_id
+  recipient_id = event.recipient_id
+  time_of_message = event.timestamp
+  message = event.message
+
+  logging.info("Received message for user %s and page %s at %s with message:"
+               % (sender_id, recipient_id, time_of_message))
+  logging.info(message)
+  page.send(sender_id, "🙏 ! I was born like yesterday 👶. So still learning how to interact" % message)
+
 
 
 @app.errorhandler(400)
@@ -39,5 +77,6 @@ def get_logs():
   return jsonify({'result': has_todays_update(progress_logs)})
 
 
+
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True, threaded=True)
