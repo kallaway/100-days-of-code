bf33e7994f566550a72edd0612b56320c31c6846
jain_nehil <jain.nehil@gmail.com>
2018-01-21 20:57:09 -0500

    added weather check functionality

--- code/brain/brain.py
+++ code/brain/brain.py
@@ -2,21 +2,23 @@ from flask import Flask, jsonify, make_response, request
 import base64
 from datetime import date, datetime, timedelta
 from pytz import timezone
-import logging
+from log import logger
 from github import Github
 from config import config
 from fbmessenger import page
+from apis.weather import get_real_feel_temp_in_celcius
 
 app = Flask(__name__)
 git = Github(config['GITHUB']['USER'], config['GITHUB']['PASSWORD'])
 _TIMEZONE = 'US/Eastern'
+_MY_RECIPIENR_ID = 1559656084121864
 
 @app.route('/fb/webhook', methods=['GET'])
 def validate():
   if request.args.get('hub.mode', '') == 'subscribe' and \
                   request.args.get('hub.verify_token', '') == config['FACEBOOK']['VERIFY_TOKEN']:
 
-    logging.debug("Validating webhook")
+    logger.debug("Validating webhook")
 
     return request.args.get('hub.challenge', '')
   else:
@@ -25,7 +27,7 @@ def validate():
 @app.route('/fb/webhook', methods=['POST'])
 def webhook():
   payload = request.get_data(as_text=True)
-  logging.debug("webhook payload: {}".format(payload))
+  logger.debug("webhook payload: {}".format(payload))
   page.handle_webhook(payload)
   return "ok"
 
@@ -60,7 +62,7 @@ def get_logs(today_str):
                   'today': today_str})
 
 def send_no_progress_update_notification():
-  my_recipient_id = 1559656084121864
+
   try:
     today = datetime.now(timezone(_TIMEZONE)).date()
     today_str = today.strftime('%b %d, %Y')
@@ -68,17 +70,32 @@ def send_no_progress_update_notification():
     time_until_eod = (datetime.combine(tomorrow, datetime.min.time()).replace(tzinfo=timezone(_TIMEZONE)) \
                       - datetime.now(timezone(_TIMEZONE)))
     hrs_til_eod = time_until_eod.seconds//3600
-    logging.info('Datetime problem {}'.format(today, tomorrow, time_until_eod))
+    logger.info('Datetime problem {}'.format(today, tomorrow, time_until_eod))
     if not is_progress_updated(today_str):
-      page.send(my_recipient_id, "No progress update for #100DaysOfCode Challenge yet! 😰~{time_left} hrs left".format(
+      page.send(_MY_RECIPIENR_ID, "No progress update for #100DaysOfCode Challenge yet! 😰~{time_left} hrs left".format(
           time_left=hrs_til_eod
       ))
       if hrs_til_eod < 2:
-        page.send(my_recipient_id, "Hurry Up")
+        page.send(_MY_RECIPIENR_ID, "Hurry Up")
     return "ok"
   except Exception as e:
-    logging.exception('Couldnt send message')
+    logger.exception('Couldnt send message')
     return "not ok"
+
+
+def send_morning_weather_update():
+  temp = get_real_feel_temp_in_celcius()[0]
+  try:
+    assert isinstance(temp, float)
+    if temp < -10 or temp > 0:
+      page.send(_MY_RECIPIENR_ID, "It feels like {} outside".format(temp))
+      return "weather_report_sent"
+    return "no_weather_report_sent"
+  except:
+    page.send(_MY_RECIPIENR_ID, "Sorry Couldn't get weather for you. #Fail")
+    return "weather_report_failure_message_sent"
+
+
 
 if __name__ == '__main__':
   app.run(debug=True, threaded=True)
