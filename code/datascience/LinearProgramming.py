15d52d0977e70ef9a2e626717203e1d9a9b17922
jain_nehil <jain.nehil@gmail.com>
2018-02-24 00:24:24 -0500

    started exploring data nut it has errors so rerunning crawler with some code cleanup

--- code/datascience/LinearProgramming.py
+++ code/datascience/LinearProgramming.py
@@ -7,15 +7,18 @@
 # 2. http://benalexkeen.com/linear-programming-with-python-and-pulp-part-1/
 # 
 
-# In[24]:
+# In[1]:
 
 
 import numpy as np
-import matplotlib.pyplot as plt
+
 import pulp
 import pandas as pd
 import re 
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
@@ -34,7 +37,7 @@ y3 = (2*x-8)/4.0
 y4 = 2 * x -5
 
 
-# In[8]:
+# In[3]:
 
 
 plt.plot(x, y1, label=r'$y\geq2$')
@@ -51,7 +54,7 @@ plt.fill_between(x, y5, y6, where=y5>y6, color='grey', alpha=0.5)
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
 
-# In[13]:
+# In[4]:
 
 
 # Now the same thing as pulp
@@ -67,34 +70,62 @@ lp_problem += 4 * y >= 2 * x - 8
 lp_problem += y <= 2 * x - 5
 
 
-# In[14]:
+# In[5]:
 
 
 lp_problem
 
 
-# In[16]:
+# In[6]:
 
 
 lp_problem.solve()
 pulp.LpStatus[lp_problem.status]
 
 
-# In[21]:
+# In[7]:
 
 
 for variable in lp_problem.variables():
     print("{} = {}".format(variable.name, variable.varValue))
 
 
-# In[22]:
+# In[8]:
 
 
 print(pulp.value(lp_problem.objective))
 
 
-# In[28]:
+# In[21]:
 
 
-books_df = pd.read_csv('data/goodreads_bestsellers.csv')
+books_df = pd.read_csv('../reading/sample_books.csv')
+
+
+# In[22]:
+
+
+books_df.head()
+
+
+# In[20]:
+
+
+sns.distplot(books_df['rating'])
+
+
+# In[23]:
+
+
+sns.distplot(books_df['num_
+# create the LP object, set up as a maximization problem --> since we want to maximize the number of books we read in a year
+prob = pulp.LpProblem('BuyingBestsellers', pulp.LpMaximize)pages'])
+
+
+# In[24]:
+
+
+# create the LP object
+# Maximization problem we want to maximize the number of books we read in 100 hours
+prob = pulp.LpProblem('RecommendedBooks', pulp.LpMaximize)
 
