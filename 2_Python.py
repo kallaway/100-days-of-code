08aa7c25d5a2409a6fa82ebe8a040454eac07015
equitymarkets <49753517+equitymarkets@users.noreply.github.com>
2019-04-19 18:46:09 +0300

    Create 2_Python.py

---
+++ 2_Python.py
@@ -0,0 +1,35 @@
+#Day 2: Using Lists			
+ 
+data = ['high-level','interpreted','1995','Guido van Rossum', 'Google','Youtube']								#some data about Python
+
+data.append('Spotify')																							                            #adds an item to the end of the list
+
+level = data[0]																									                                #list of elements defined as variables
+style = data[1]																										
+pyth = data[2]
+founder = data[3]
+website1 = data[-3]
+website2 = data[-2]
+website3 = data[-1]
+
+input()																											                                    #waits for user to press enter
+
+print("Python is a/an " + level + " programming language. It is " + style + 									  #concatenates the data into a readable sentence
+".\nIt first appeared in the year " + pyth + 
+" and was founded by " + founder + ".\nIt is used by many large projects, including " 
++ website1 + ", " + website2 + ", and " 
++ website3 + ".")
+
+input("\nPress enter to continue.")																				                      #waits for user to press enter
+
+input("\nIn the piece 'The Zen of Python', Tim Peters oulines a general mindset for Python programmers.\n")		#introduces following piece
+
+import this 																									                                  #Zen of Python
+
+input()																											                                    #waits for user to press enter
+
+print("Bye-bye")																								                                #Bye-bye cmd
+
+import antigravity																								                              #website
+
+input()																											                                    #waits for user to press enter
