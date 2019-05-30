de9812457f0145c46c76e8175167be979ccf0cae
Derek <49753517+equitymarkets@users.noreply.github.com>
2019-05-30 15:51:30 +0300

    Update 43_SeabornHeatmap.py

--- 43_SeabornHeatmap.py
+++ 43_SeabornHeatmap.py
@@ -1,5 +1,21 @@
 #Day 43: Heat Map Cont. 
-#
+#Here we have the corrected heatmap and clustermap scripts
 #
 
 import seaborn as sns
+import numpy as np
+#import fastcluster 
+
+#read the csv
+
+ans = sns.load_dataset('anscombe')
+
+anspiv = ans.pivot('dataset', 'y', 'x')
+#heatmap = sns.heatmap(anspiv, square=True, linecolor='purple')
+heat = sns.heatmap(anspiv)
+
+print(' ')
+
+#clusterpiv = ans.pivot('y', 'x')
+data = ans.pop('dataset')
+cluster = sns.clustermap(ans)
