0578d482b2f5e8a489d6fc71cb38d9500cc9ac8a
Derek <49753517+equitymarkets@users.noreply.github.com>
2019-05-31 15:50:25 +0300

    Create 44_SeabornJointPlot.py

---
+++ 44_SeabornJointPlot.py
@@ -0,0 +1,7 @@
+#Day 44: Misc. Seaborn 
+#Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+#Type "help", "copyright", "credits" or "license()" for more information.
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
