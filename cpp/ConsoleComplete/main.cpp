af3f96e490d739c261d31648ff343cc2db77fdc0
eshiofune <ehioroboevans@gmail.com>
2019-10-16 19:46:29 +0100

    Updated the log for today

--- cpp/ConsoleComplete/main.cpp
+++ cpp/ConsoleComplete/main.cpp
@@ -15,24 +15,22 @@ bool contains(string str1, string str2) {
 	return (str1.find(str2) != string::npos);
 }
 
-string getMatch(string partialWord) {
-	// returns the closest match in the DICTIONARY to partialWord
+string getMatch(string text) {
+	// returns the closest match in the DICTIONARY to text
 	
 	for (int i = 0; i < 4; i++) {
-		if (contains(DICTIONARY[i], partialWord)) {
+		if (contains(DICTIONARY[i], text)) {
 			return DICTIONARY[i];
 		}
 	}
 	
-	return partialWord;
+	return text;
 }
 
 int main(int argc, char** argv) {
 	string input, closestMatch;
-	int inputLength;
 	
 	cin >> input;
-	inputLength = input.length();
 	
 	closestMatch = getMatch(input);
 	cout << closestMatch << "\n\n\n\n\n\n";
