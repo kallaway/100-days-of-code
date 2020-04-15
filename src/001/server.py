36ad6855e927ee23cd2c3c96869f2012cd19bec7
jstoveld <jstoveld@gmail.com>
2020-04-15 13:24:56 -0400

    cleaned up comments.

--- src/001/server.py
+++ src/001/server.py
@@ -12,4 +12,5 @@ if __name__ == "__main__":
     app.run(host='0.0.0.0',port='3000')
 
 # __dunder__ this is called a dunder
-# Dunder or magic methods in Python are the methods having two prefix and suffix underscores in the method name. Dunder here means “Double Under (Underscores)”. These are commonly used for operator overloading. 
+# Dunder or magic methods in Python are the methods having two prefix and suffix underscores in the method name. 
+# Dunder here means “Double Under (Underscores)”. These are commonly used for operator overloading. 
