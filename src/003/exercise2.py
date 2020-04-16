51c9492fb6ce75d0786f8e01c352fb5631feacb9
jstoveld <jstoveld@gmail.com>
2020-04-16 18:58:09 -0400

    Checking in notes from todays work, and starting exercise 2.

---
+++ src/003/exercise2.py
@@ -0,0 +1,22 @@
+# task 1
+#if the first letter of the word is a costenant move that to the end and add "ay"
+#if the first letter of the word is a vowel - just add "way" to the end.
+
+#task 2
+# function named numerus
+#take roman numerals and output arabic numerals
+# ie: xiv => 14
+#mmxlix => 2049
+#
+#i = 1
+#v = 5
+#x = 10
+#l = 50
+#c = 100
+#m = 1000
+# unless a number below it is immedately preceding it
+# eg iv = 4
+# eg cm = 900
+
+def igpay
+
