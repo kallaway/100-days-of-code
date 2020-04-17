90147f269830882b6c57e3ec4dd3a169ab18a93a
jstoveld <jstoveld@gmail.com>
2020-04-16 20:25:28 -0400

    Moved next exercise over to new file.

--- src/003/exercise2.py
+++ src/003/exercise2.py
@@ -18,5 +18,45 @@
 # eg iv = 4
 # eg cm = 900
 
-def igpay
+#Task 1
+#We need to define a word
+a_word = "working
 
+#[0] will define our starting letter
+a_word[0]
+#W will be 0
+
+a_word[1: ] + a_word[0]
+#takes orking and adds the w
+
+a_word[1: ] + a_word[0] + "ay"
+#adds ay at the end
+
+b_word = "oranges"
+#word b has a vowel at the front
+
+b_word + "way"
+#adds way to the end
+
+
+vowels = "aeiou"
+#defining vowels
+
+"a" in vowels
+#test vowel
+
+if a_word[0] in vowels:
+    print(a_word + "way")
+else:
+    print(a_word[1: ] + a_word[0] + "ay")
+#adding way to the end of a words
+
+
+def igpay(a_word):
+    vowels = "aeiou"
+    
+    if a_word[0] in vowels:
+        print(a_word + "way")
+    else:
+        print(a_word[1: ] + a_word[0] + "ay")
+#creating the function
