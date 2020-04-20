26c24197ca8b9fce86f9a5f5633aeb37f822ad97
jstoveld <jstoveld@gmail.com>
2020-04-20 17:14:10 -0400

    Added content for HW assignment.

---
+++ src/007/homework.py
@@ -0,0 +1,4 @@
+##Without sets write a function that is called intersect that takes 2 lists and returns the lists commonalities
+## How is interset different from list(set(list_a)).intersection(list_b)
+#Write a function "pair" that takes two lists and prints the values from each list "paired" together seperated by commas
+#How is 'pair' different than  for (x,y) in zip(list_a, list_b): print "{0}, {I}".format(x,y)
