1f0af256db5054f23a8a7092a15069ed6adb474a
SharonTahar <sharontahar@gmail.com>
2020-10-10 14:21:28 +0300

    #20Day

--- dnd/src/App.js
+++ dnd/src/App.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
-// import Form3 from './form/Form3';
+import Form4 from './form/Form4';
 import Board1 from './components/Board1';
 import './App.css';
 
 function App() {
   return (
     <div>
-        <Board1 />
+        <Form4 />
     </div>
   );
 }
