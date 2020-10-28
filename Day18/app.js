916c8f32e504737c45bdcd39e00b4bf010cef625
William Hall <billhall168@gmail.com>
2020-10-28 16:28:18 -0700

    start javascript file

--- Day18/app.js
+++ Day18/app.js
@@ -1,1 +1,9 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function() {
+console.log(document.body);
+});
+
