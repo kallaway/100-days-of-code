3ab328ba1ed7ad63baf94a70dcb38540a6fc7745
William Hall <billhall168@gmail.com>
2020-11-09 03:26:11 -0700

    include special age group

--- Day24/ride.py
+++ Day24/ride.py
@@ -11,6 +11,8 @@ if height >= 120:
     elif age <= 18:
         bill = 7
         print("Youth tickets are $7.00")
+    elif age >= 45 and age <= 55:
+        print("Everything will be ok. This ride is on us.")
     else: 
         bill = 12
         print("Adult tickets are $12.00")
