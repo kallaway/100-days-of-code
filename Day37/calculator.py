51756e0abcf5564c6868ec79fc7fdffb8ba7c047
William Hall <billhall168@gmail.com>
2020-11-26 06:24:41 -0700

    create calculator dictionary

--- Day37/calculator.py
+++ Day37/calculator.py
@@ -1,1 +1,47 @@
 import Day37.art
+
+
+
+
+
+
+
+
+
+# Create a dictionary
+calculator_functions = {
+    "+": add,
+    "-": subtract,
+    "x": multiply,
+    "/": divide,
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Calculator
+
+# Add
+def add(n1, n2):
+    return n1 + n2
+
+# Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+# Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+# Divide
+def divide(n1, n2):
+    return n1 / n2
