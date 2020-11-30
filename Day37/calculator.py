c94ff7dccbeaf3ac40670a9c841e1faf3505999e
William Hall <billhall168@gmail.com>
2020-11-30 02:06:50 -0700

    last of day 38 calculator

--- Day37/calculator.py
+++ Day37/calculator.py
@@ -30,20 +30,20 @@ calculator_functions = {
 
 def calculator():
 
-    num1 = int(input("What is the first number?\n"))
+    num1 = float(input("What is the first number?\n"))
     for symbol in calculator_functions:
         print(symbol)
     should_continue = True
 
     while should_continue: 
         symbol_operator = input("Pick an operation from the lines above: ")
-        num2 = int(input("What is the next number?\n"))
+        num2 = float(input("What is the next number?\n"))
         calculate = calculator_functions[symbol_operator]
         answer = calculate(num1, num2)
 
         print(f"{num1} {symbol_operator} {num2} = {answer}")
 
-        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' start a new calculation.: ") == "y":
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' start a new calculation.\n") == "y":
             num1 = answer
         else:
             should_continue = False
