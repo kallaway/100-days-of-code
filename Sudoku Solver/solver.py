e628845a375ee87f0892331554bb064baf611800
AbsurdCactus <39064400+AbsurdCactus@users.noreply.github.com>
2020-12-02 14:19:18 -0500

    R2D2

--- Sudoku Solver/solver.py
+++ Sudoku Solver/solver.py
@@ -1,38 +1,34 @@
 import numpy as np
 
-grid = [[0,0,3,7,0,0,0,9,0],
-        [1,0,6,4,0,0,0,0,0],
-        [4,0,5,2,0,0,0,0,0],
-        [0,4,0,0,0,0,3,0,0],
-        [7,5,0,0,0,0,0,8,1],
-        [0,0,9,0,0,0,0,5,0],
-        [0,0,0,0,0,4,9,0,2],
-        [0,0,0,0,0,9,7,0,8],
-        [0,3,0,0,0,8,5,0,0]]
-# grid = [[0,0,3,7,0,0,0,9,0],
-#         [1,0,6,4,0,0,0,0,0],
-#         [4,0,5,2,0,0,0,0,0],
-#         [0,4,0,0,0,0,3,0,9],
-#         [7,5,0,0,0,0,0,8,1],
-#         [0,0,9,0,0,0,0,5,0],
-#         [0,0,0,0,0,4,9,0,2],
-#         [0,0,0,0,0,9,7,0,8],
-#         [0,3,0,0,0,8,5,0,0]]
+# Sudoku Solver
+# Puzzle I used to test: https://www.websudoku.com/?level=4&set_id=3017885857
+
+grid = [[0, 0, 3, 7, 0, 0, 0, 9, 0],
+        [1, 0, 6, 4, 0, 0, 0, 0, 0],
+        [4, 0, 5, 2, 0, 0, 0, 0, 0],
+        [0, 4, 0, 0, 0, 0, 3, 0, 0],
+        [7, 5, 0, 0, 0, 0, 0, 8, 1],
+        [0, 0, 9, 0, 0, 0, 0, 5, 0],
+        [0, 0, 0, 0, 0, 4, 9, 0, 2],
+        [0, 0, 0, 0, 0, 9, 7, 0, 8],
+        [0, 3, 0, 0, 0, 8, 5, 0, 0]]
 
 
-def is_possible(y,x,n):
+
+def is_possible(y, x, n):
+    # 'global' makes changes to the variable not exlusive to the function
     global grid
-    for i in range(0,9):
+    for i in range(0, 9):
         if grid[y][i] == n:
             return False
-    for i in range(0,9):
+    for i in range(0, 9):
         if grid[i][x] == n:
             return False
     x0 = (x//3)*3
     y0 = (y//3)*3
-    for i in range(0,3):
-        for j in range(0,3):
-            if grid[y0+i][x0+j]==n:
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if grid[y0+i][x0+j] == n:
                 return False
     return True
     # '//' divides, then rounds to the 'floor' of the number
@@ -43,9 +39,19 @@ def solve():
     for y in range(9):
         for x in range(9):
             if grid[y][x] == 0:
-                for n in range(1,10):
-                    if is_possible(y,x,n):
+                for n in range(1, 10):
+                    if is_possible(y, x, n):
+                        grid[y][x] = n
+                        solve()
+                        # If we reach a dead end and cannot complete the solution,
+                        # we take all of our changes out (backtracking)
+                        grid[y][x] = 0
+                # and then we return
+                return
+    print(np.matrix(grid))
+    input("More?")
 
 
-print(np.matrix(grid))
-print(is_possible(3,8,9))
+# print(np.matrix(grid))
+# print(is_possible(3, 8, 9))
+print(solve())
--- r2-log.md
+++ r2-log.md
@@ -7,4 +7,9 @@ The log of my #100DaysOfCode challenge. Started on [December 1st, Monday, 2020].
 ### R2D1 
 Started a Sudoku Solver. Had help from Youtube:https://www.youtube.com/watch?v=G_UYXzGuqvM. Coded a function which can tell if a supposed entry in a Sudoky puzzle is possible.
 
-### R2D2
+### R2D2 (12/2/20)
+Finished the Sudoky Solver. It was really fun and I might play with said file another time to better understand the recursion process. Guy in video proposed a project idea of a puzzle from his childhood (example: https://www.schoolarchimedes.com/klotski). I might revisit this project idea in time. 
+
+Started tutorial on how to use Docker. Since I often switch between using my laptop and desktop, I consider this a prime opportinity to get more frequent with preserving development environments in an efficient mannter. Tutorial can be found here: https://docs.docker.com/compose/gettingstarted/. I'll apply a Docker file to my previous Sudoku project once I have the hang of it.
+
+ALSO, almost forgot, but added development branch to my repository. I will be posting my in-progress stuff there from now on. 
