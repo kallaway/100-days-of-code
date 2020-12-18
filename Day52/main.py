f73bbffa8cca56880cb35ebd39afa28aad4e588f
William Hall <billhall168@gmail.com>
2020-12-18 03:51:01 -0700

    draw various shapes and change line colors randomly

--- Day52/main.py
+++ Day52/main.py
@@ -1,8 +1,8 @@
 from turtle import Turtle, Screen
+import random
 
 tatiana = Turtle()
 tatiana.shape("turtle")
-tatiana.color("red")
 
 # draw a square
 # for _ in range(4):
@@ -11,24 +11,26 @@ tatiana.color("red")
 
 # draw a dashed line
 
-for _ in range(15):
-    tatiana.forward(10)
-    tatiana.penup()
-    tatiana.forward(10)
-    tatiana.pendown()
+# for _ in range(15):
+#     tatiana.forward(10)
+#     tatiana.penup()
+#     tatiana.forward(10)
+#     tatiana.pendown()
+
+colors = ["teal", "burlywood", "dark turquoise", "firebrick", "medium violet red", "pale violet red", "blue violet",
+          "sea green"]
 
 
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tatiana.forward(100)
+        tatiana.right(angle)
 
 
-
-
-
-
-
-
-
-
-
+for shape_side_n in range(3, 11):
+    tatiana.color(random.choice(colors))
+    draw_shape(shape_side_n)
 
 screen = Screen()
 screen.exitonclick()
