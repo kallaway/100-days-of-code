5cd965b751ec1009e075ff7ae5c6d6d30a80a647
William Hall <billhall168@gmail.com>
2020-12-24 02:42:35 -0700

    create food and make it move randomly when snake hits it

---
+++ Day57/main.py
@@ -0,0 +1,32 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_running = True
+while game_running:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # check for food collision
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
+
+screen.exitonclick()
