969a410bdb072368806b312c6d4729837c7f4414
William Hall <billhall168@gmail.com>
2020-12-24 03:05:27 -0700

    create scoreboard and update when snake hits it

--- Day57/main.py
+++ Day57/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -27,6 +29,6 @@ while game_running:
     # check for food collision
     if snake.head.distance(food) < 15:
         food.refresh()
-
+        scoreboard.increase_score()
 
 screen.exitonclick()
---
+++ Day57/scoreboard.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 260)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+
