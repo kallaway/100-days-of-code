13fd1a5b53e9c5dfa7b1e33549deee7030b48e9a
William Hall <billhall168@gmail.com>
2020-12-28 02:20:29 -0700

    Start pong game. position paddle, define up and down functions

---
+++ Day59/main.py
@@ -0,0 +1,32 @@
+from turtle import Screen
+from turtle import Turtle
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+
+screen.exitonclick()
