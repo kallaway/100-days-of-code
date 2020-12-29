7495e05a31f15ca59553272ec941ea5a28d40042
William Hall <billhall168@gmail.com>
2020-12-29 03:15:03 -0700

    final pong game

--- Day59/main.py
+++ Day59/main.py
@@ -1,7 +1,8 @@
-from turtle import Screen, Turtle
-from paddle import Paddle
+from turtle import Screen
+
 from ball import Ball
-import time
+from paddle import Paddle
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -11,21 +12,36 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
-
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
-# research ways to make paddle scroll
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
-
-game_on = True
-while game_on:
-    time.sleep(0.1)
-    ball.move()
+game_is_on = True
+while game_is_on:
     screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect L paddle misses:
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
--- Day59/paddle.py
+++ Day59/paddle.py
@@ -1,7 +1,8 @@
 from turtle import Turtle
 
 
-class Paddle (Turtle):
+class Paddle(Turtle):
+
     def __init__(self, position):
         super().__init__()
         self.shape("square")
