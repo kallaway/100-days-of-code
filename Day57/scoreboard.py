5fb73e411c8d00d09ce90065b13c09134b64ef52
William Hall <billhall168@gmail.com>
2021-01-01 05:08:06 -0700

    update snake game to store high score in text file.

--- Day57/scoreboard.py
+++ Day57/scoreboard.py
@@ -9,7 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -23,6 +24,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", "w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
