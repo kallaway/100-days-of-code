edb378035067b2049731ecba9f5b81e7c08de109
William Hall <billhall168@gmail.com>
2021-01-07 02:58:32 -0700

    create own dataframe

--- Day67/main.py
+++ Day67/main.py
@@ -1,5 +1,30 @@
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(type(data))
+# data = pandas.read_csv("weather_data.csv")
+
+# columns are Series
+# print(type(data))
 # print(data["temp"])
+
+# data_dict = data.to_dict()
+# print(data_dict)
+
+
+# print(data["temp"].max())
+# print(data["condition"])
+# print(data.condition)
+
+# find rows
+# print(data[data.temp == data.temp.max()])
+
+# monday = (data[data.day == "Monday"])
+# print(monday.temp * 2 + 30)
+
+# create a dataframe
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
