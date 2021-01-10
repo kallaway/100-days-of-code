501823b377865d93f5b144f497341c75e51626b9
William Hall <billhall168@gmail.com>
2021-01-10 02:52:26 -0700

    guess the state capital game

--- Day68/main.py
+++ Day68/main.py
@@ -1,7 +1,6 @@
 import turtle
 import pandas
 
-
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
@@ -9,19 +8,25 @@ screen.addshape(image)
 turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
-all_states = data.state.to_list()
+all_capitals = data.capital.to_list()
+guessed_capitals = []
 
-answer_state = screen.textinput(title="Guess the State", prompt="Enter a State name")
-
-print(answer_state.title())
-
-if answer_state in all_states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    t.goto(int(state_data.x), int(state_data.y))
-    t.write(state_data.state)
-
-
-screen.exitonclick()
+while len(guessed_capitals) < 50:
+    answer_capital = screen.textinput(title=f"{len(guessed_capitals)}/50 Capitals Correct",
+                                      prompt="What's another state's capital?").title()
+    if answer_capital == "Exit":
+        missing_states = []
+        for state in all_capitals:
+            if state not in guessed_capitals:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_capital in all_capitals:
+        guessed_capitals.append(answer_capital)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        capitals_data = data[data.capital == answer_capital]
+        t.goto(int(capitals_data.x), int(capitals_data.y))
+        t.write(answer_capital)
