3b9d5aefad60ddb0910c6c5568e81564841e17d2
William Hall <billhall168@gmail.com>
2021-01-12 02:29:59 -0700

    Create a new dictionary from another dictionary

---
+++ Day71/main.py
@@ -0,0 +1,17 @@
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+# 🚨 Don't change code above 👆
+
+
+# Write your code 👇 below:
+weather_f = {day: temp_c * 9/5 + 32 for (day, temp_c) in weather_c.items()}
+
+
+print(weather_f)
