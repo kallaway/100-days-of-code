e6e8c8016b7b498a9898e85c798d619d99d170ae
William Hall <billhall168@gmail.com>
2021-01-14 03:01:13 -0700

    working with tkinter labels, button, and entries

--- Day72/playground.py
+++ Day72/playground.py
@@ -7,3 +7,22 @@ def add(*nums):
 
 
 print(add(1, 2, 3, 4))
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+
+    print(n)
+
+calculate(2, add=3, multiply=5)
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+
+my_car = Car(make="Nissan")
+print(my_car.model)
