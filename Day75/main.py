e794a4d967002c7fa0c7238d9ea81112fc35e68e
William Hall <billhall168@gmail.com>
2021-01-20 02:56:42 -0700

    add label and start and reset buttons

--- Day75/main.py
+++ Day75/main.py
@@ -20,11 +20,20 @@ window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 45))
+timer_label.grid(column=1, row=0)
+
+# can use highlightthickness=0 to remove white background
 canvas = Canvas(width=200, height=224, bg=YELLOW)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(102, 112, image=tomato_img)
 canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+start_button = Button(text="Start", bg=YELLOW)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", bg=YELLOW)
+reset_button.grid(column=2, row=2)
 
 window.mainloop()
