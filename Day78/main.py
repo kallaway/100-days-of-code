5d54c0eeb107c8566a09f403a95852b8aa582fff
William Hall <billhall168@gmail.com>
2021-01-22 07:37:24 -0700

    add simple form validation

--- Day78/main.py
+++ Day78/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -9,11 +9,18 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {email} | {password}\n")
-        # want to sort items alphabetically
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="oops", message="Please enter valid entries for email and password")
+    else:
+
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \n "
+                                                        f"\nPassword: {password} \nIs it okay to save?")
+        if is_ok:
+                with open("data.txt", "a") as data_file:
+                    data_file.write(f"{website} | {email} | {password}\n")
+                    # want to sort items alphabetically
+                    website_entry.delete(0, END)
+                    password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
