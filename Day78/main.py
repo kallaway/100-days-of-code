abd51fc96381952ae1672c85b8622a92b772230e
William Hall <billhall168@gmail.com>
2021-01-22 08:07:39 -0700

    add password generator code from day 5 udemy python course

--- Day78/main.py
+++ Day78/main.py
@@ -1,7 +1,28 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -14,13 +35,13 @@ def save():
     else:
 
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \n "
-                                                        f"\nPassword: {password} \nIs it okay to save?")
+                                                              f"\nPassword: {password} \nIs it okay to save?")
         if is_ok:
-                with open("data.txt", "a") as data_file:
-                    data_file.write(f"{website} | {email} | {password}\n")
-                    # want to sort items alphabetically
-                    website_entry.delete(0, END)
-                    password_entry.delete(0, END)
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                # want to sort items alphabetically
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -53,7 +74,7 @@ password_label.grid(row=3, column=0)
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
-generate_button = Button(text="Generate Password")
+generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36, command=save)
