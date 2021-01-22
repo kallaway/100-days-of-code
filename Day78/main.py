1ddba0dae817ecf02beb4933af3e8e4cba711380
William Hall <billhall168@gmail.com>
2021-01-22 06:12:39 -0700

    ui setup for password manager

---
+++ Day78/main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(column=1, row=1)
+
+window.mainloop()
