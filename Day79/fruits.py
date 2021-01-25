f2ca7cc3aaf3672d80746fc5a7a08c3055733ea0
William Hall <billhall168@gmail.com>
2021-01-25 03:10:14 -0700

    use pass to skip keyerror

---
+++ Day79/fruits.py
@@ -0,0 +1,14 @@
+fruits = ["Apple", "Pear", "Orange"]
+
+
+# TODO: Catch the exception and make sure the code runs without crashing.
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+    except IndexError:
+        print("Fruit pie")
+    else:
+        print(fruit + " pie")
+
+
+make_pie(4)
---
+++ Day79/posts.py
@@ -0,0 +1,19 @@
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
+
+total_likes = 0
+
+for post in facebook_posts:
+    try:
+        total_likes = total_likes + post['Likes']
+    except KeyError:
+        pass
+
+
+print(total_likes)
