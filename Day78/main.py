88f7054e834559ee06cf021005ce37593adb695f
William Hall <billhall168@gmail.com>
2021-01-25 15:27:10 -0700

    save passwords to json

--- Day78/main.py
+++ Day78/main.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 # Password Generator Project
@@ -29,19 +31,26 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="oops", message="Please enter valid entries for email and password")
     else:
+        with open("data.json", "r") as data_file:
 
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \n "
-                                                              f"\nPassword: {password} \nIs it okay to save?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                # want to sort items alphabetically
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+            data = json.load(data_file)
+            data.update(new_data)
+
+        with open("data.json", "w") as data_file:
+            json.dump(data, data_file, indent=4)
+            # want to sort items alphabetically
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
