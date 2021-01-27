5a486f016d584d2e00e6f75ed32f9e577d816bf4
William Hall <billhall168@gmail.com>
2021-01-27 02:09:39 -0700

    upgrade password manager

--- Day78/main.py
+++ Day78/main.py
@@ -58,6 +58,23 @@ def save():
             password_entry.delete(0, END)
 
 
+# --------------------------------- FIND PASSWORD ----------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword:  {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exist.")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -71,9 +88,12 @@ canvas.grid(row=0, column=1)
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
+
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
