eb3e9c72a7039a5b27f4c87bcaf3ab8f7625b3a5
William Hall <billhall168@gmail.com>
2021-01-28 02:46:33 -0700

    flip function working

--- Day81/main.py
+++ Day81/main.py
@@ -1,29 +1,40 @@
 from tkinter import *
 import pandas
 import random
+
 BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("data/spanish_words.csv")
 need_to_learn = data.to_dict(orient="records")
-print(need_to_learn)
+current_card = {}
 
 
 def new_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(need_to_learn)
-
     canvas.itemconfig(card_title, text="Spanish")
-    canvas.itemconfig(card_word, text=current_card["Spanish"])
+    canvas.itemconfig(card_word, text=current_card["Spanish"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
-    # Dataframe.to_dict(orient="records")
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
 
 # ***********************************  UI SETUP ************************************
 window = Tk()
 window.title("Spanish Translator")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
@@ -41,4 +52,3 @@ right_button.grid(row=1, column=1)
 new_card()
 
 window.mainloop()
-
