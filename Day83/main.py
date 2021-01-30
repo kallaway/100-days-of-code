d22e663e85a14ebf05b06beb423d888551c3e130
William Hall <billhall168@gmail.com>
2021-01-30 03:17:15 -0700

    open random quote

--- Day83/main.py
+++ Day83/main.py
@@ -1,5 +1,6 @@
 import smtplib
 
+# https://stackoverflow.com/questions/10147455/how-to-send-an-email-with-gmail-as-provider-using-python
 fromaddr = 'christianwebdeveloper57@gmail.com'
 toaddrs = 'billhall105@yahoo.com'
 msg = "\r\n".join([
---
+++ Day84/main.py
@@ -0,0 +1,12 @@
+import smtplib
+import datetime as dt
+import random
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 5:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
