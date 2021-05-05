011922ca1498b781017ad02e2222f4071146752b
Moe Chowdhury <moe.chowdhury@outlook.com>
2021-05-05 14:38:15 -0400

    Completed sortedSquares

--- Days/Day2/codingChallenge.js
+++ Days/Day2/codingChallenge.js
@@ -12,3 +12,18 @@ var findNumbers = function(nums) {
 
 nums = [555,901,482,1771];
 console.log(findNumbers(nums));
+
+/* Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order. */
+var sortedSquares = function(nums) {
+    var cache = []
+    for(var i = 0; i < nums.length; i++){
+        cache.push(nums[i]*nums[i])
+    }
+    cache.sort(function(a, b) {
+        return a - b;
+    });
+    return cache;
+};
+
+nums = [-7,-3,2,3,11];
+console.log(sortedSquares(nums))
