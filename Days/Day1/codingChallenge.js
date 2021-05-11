9a1e0f0da6c484956f4472555a1b1c2669c56fd5
Moe Chowdhury <moe.chowdhury@outlook.com>
2021-05-11 15:26:15 -0400

    Rename Days

--- Days/Day1/codingChallenge.js
+++ Days/Day1/codingChallenge.js
@@ -1,17 +1,29 @@
-/* Find Max Consecutive Ones in a Given Array*/
-var findMaxConsecutiveOnes = function(nums) {
-    var cache = [];
+/* Find Numbers with Even Number of Digits */
+var findNumbers = function(nums) {
     var counter = 0;
-    for(var i =0; i <= nums.length;i++){
-        if(nums[i] === 1){
+    for(var i = 0; i < nums.length; i++){
+        var eachElementLength = `${nums[i]}`.split('').length
+        if(eachElementElement % 2 === 0){
             counter++;
-        } else {
-            cache.push(counter);
-            counter = 0;
         }
     }
-    return Math.max(...cache);
+    return counter;
 };
 
-nums = [1,0,1,1,0,1,1,1]
-console.log(findMaxConsecutiveOnes(nums))
+nums = [555,901,482,1771];
+console.log(findNumbers(nums));
+
+/* Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order. */
+var sortedSquares = function(nums) {
+    var cache = []
+    for(var i = 0; i < nums.length; i++){
+        cache.push(nums[i]*nums[i])
+    }
+    cache.sort(function(a, b) {
+        return a - b;
+    });
+    return cache;
+};
+
+nums = [-7,-3,2,3,11];
+console.log(sortedSquares(nums))
