29d1f95296f21270bc93152b73f0b54b13b82c6d
Moe Chowdhury <moe.chowdhury@outlook.com>
2021-05-11 17:09:33 -0400

    solved merge sorted array

--- Days/Day2/codingChallenge.js
+++ Days/Day2/codingChallenge.js
@@ -10,3 +10,13 @@ var duplicateZeros = function(arr) {
 
 var arr = [1,0,2,3,0,4,5,0]
 console.log(duplicateZeros(arr))
+
+/* Merge Sorted Array */
+var merge = function(nums1, m, nums2, n) {
+    for (var i = 0; i < n; i++) {
+            nums1[i + m] = nums2[i];
+    }
+    nums1.sort(function(a, b) {
+        return a - b;
+    });
+};
