9e33a3b2979669780a753ac72e7d40dfa36533a0
Moe Chowdhury <moe.chowdhury@outlook.com>
2021-05-20 06:10:19 -0400

    Day 4 Challenges

---
+++ Days/Day4/codingChallenge.js
@@ -0,0 +1,18 @@
+var fullJustify = function(words, maxWidth) {
+    //iterate through elements in the array
+        //add length to counter and keep adding until its less than maxWidth including width
+        //add to string var and push to array
+
+    for(var i = 0; i < words.length; i++){
+        var counter = 0;
+        while(counter <= maxWidth){
+            var cache = '';
+            
+        }
+    }
+};
+
+var words = ["This", "is", "an", "example", "of", "text", "justification."];
+var maxWidth = 16;
+console.log(fullJustify(words, maxWidth))
+
