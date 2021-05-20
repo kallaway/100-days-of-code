9ccc5d9152e7de9c2fa112ca641bee9f850f4455
Moe Chowdhury <moe.chowdhury@outlook.com>
2021-05-20 06:15:27 -0400

    Day 4 Completion

--- Days/Day4/codingChallenge.js
+++ Days/Day4/codingChallenge.js
@@ -1,3 +1,5 @@
+/*
+
 var fullJustify = function(words, maxWidth) {
     //iterate through elements in the array
         //add length to counter and keep adding until its less than maxWidth including width
@@ -5,9 +7,10 @@ var fullJustify = function(words, maxWidth) {
 
     for(var i = 0; i < words.length; i++){
         var counter = 0;
-        while(counter <= maxWidth){
+        if(counter <= maxWidth){
             var cache = '';
-            
+            counter + words[i].length;
+            cache += words[i]
         }
     }
 };
@@ -16,3 +19,32 @@ var words = ["This", "is", "an", "example", "of", "text", "justification."];
 var maxWidth = 16;
 console.log(fullJustify(words, maxWidth))
 
+*/
+
+function hasDuplicates(array) {
+    return (new Set(array)).size !== array.length;
+}
+
+var lengthOfLongestSubstring = function(s) {
+    var str = s.split('');
+    var cache = [];
+    var count = [];
+    if(s.length === 0){
+        return 0;
+    }
+    for(var i = 0; i < str.length; i++){
+        cache.push(str[i]);
+        if(hasDuplicates(cache) === false){
+            console.log(cache)
+            console.log(cache.length)
+            count.push(cache.length)
+            console.log(count)
+        } else {
+            cache = [];
+        }
+    }
+    return Math.max(...count)
+};
+
+var s = 'aab';
+console.log(lengthOfLongestSubstring(s))
