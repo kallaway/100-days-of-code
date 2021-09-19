043cabc0337a5ee001042776c0988abe0e4a7bf3
HymieHerrera <58313376+HymieHerrera@users.noreply.github.com>
2021-09-19 04:56:36 -0500

    Adding 100

---
+++ 100 Days/BandNameGenerator.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("Hello! Music Lovers.  Let's figure out the name of your band.")
+#2. Ask the user for the city that they grew up in.
+city = input ("Where are you from? ")
+#3. Ask the user for the name of a pet.
+pet = input ("What is the name of your favorite pet? ")
+#4. Combine the name of their city and pet and show them their band name.
+print ("The name of your band is " + city + " " + pet)
