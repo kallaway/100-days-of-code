c3e8acd3483bcc862d9e1ff8ab82c321260fbeb6
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2022-01-29 20:04:25 +0800

    [ADD] array methods

---
+++ Day-6/reverse-method-in-array.js
@@ -0,0 +1,5 @@
+arr = ['b', 'e', 'a', 'o', 'p', 'n', 'r'];
+console.log(arr);
+
+arr.reverse();
+console.log(arr);
---
+++ Day-6/unshift-method-of-array.js
@@ -0,0 +1,5 @@
+var arr = [5, 5, 8, 7, 6];
+console.log(arr);
+
+arr.unshift(1);
+console.log(arr);
