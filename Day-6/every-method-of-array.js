e0d8ec4dc7da63fafd53b6cce338bb4d7496b713
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2022-01-29 20:25:26 +0800

    [ADD] more difficult methods like filter, find, reduce, join, for each, slice and every method of array in node

---
+++ Day-6/every-method-of-array.js
@@ -0,0 +1,12 @@
+var arr = [2, 4, 6, 8, 10];
+var arr1 = [2, 3, 4, 6, 8];
+
+function even(value) {
+  return (value % 2) == 0;
+}
+
+var out = arr.every(even);
+var out1 = arr1.every(even);
+
+console.log("Output of an array 1: " + out);
+console.log("Output of an array 2: " + out1);
---
+++ Day-6/join-method-of-array.js
@@ -0,0 +1,10 @@
+arr = [5, 2, 8, 5, 6];
+arr1 = ['a', 'b', 'c', 'd'];
+console.log(arr);
+console.log(arr1);
+
+var str = arr.join();
+var str1 = arr1.join(':');
+
+console.log(str);
+console.log(str1);
