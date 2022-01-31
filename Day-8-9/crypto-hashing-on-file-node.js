0140505e50907813473be474cc3a7afcbd2904c6
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2022-01-31 22:38:18 +0800

    [ADD] crypto hashing and encrypt the files

---
+++ Day-8-9/crypto-hashing-on-file-node.js
@@ -0,0 +1,22 @@
+var crypto = require('crypto');
+var fs = require('fs');
+
+var algorithm = 'sha256';
+
+var hash = crypto.createHash(algorithm); 
+
+var filename = 'data.txt';
+
+var file_data = fs.ReadStream(filename);
+
+file_data.on('data', function(data){
+  hash.update(data);
+});
+
+file_data.on('end', function() {
+  var gen_hash = hash.digest('hex');
+  console.log('Hash generated using ' + algorithm + '\nHashed output is : ' + gen_hash + ' \nFile name is : ' + filename);
+
+  fs.writeFileSync(filename, gen_hash);
+});
+
