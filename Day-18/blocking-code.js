a3e0ec2c482d0422d4a1a355f555755d908a4c82
khangaikhuu uvgunkhuu <khangaikhuu@mstars.mn>
2022-02-07 18:23:01 +0800

    [ADD] blocking code with output txt file

--- Day-18/blocking-code.js
+++ Day-18/blocking-code.js
@@ -3,4 +3,21 @@ var fs = require('fs');
 var date1 = new Date();
 var time_start = date1.getTime();
 
+console.log('starting at: ' + time_start);
+console.log("Let's start reading file");
 
+var filename = 'output.txt';
+
+var content = fs.readFileSync(filename);
+
+console.log('Content: ' + content);
+
+var date2 = new Date();
+var time_end = date2.getTime();
+
+console.log('finishing at: ' + time_end);
+var exection_time = time_end - time_start;
+console.log("Time for execution: " + exection_time);
+
+
+console.log('Another task to be executed');
