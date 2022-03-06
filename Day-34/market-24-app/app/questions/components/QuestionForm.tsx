dc5891bbc0144a0d79a0fac862f7458c9f29b5fa
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2022-03-06 16:05:26 +0800

    [ADD] questions form tsx

--- Day-34/market-24-app/app/questions/components/QuestionForm.tsx
+++ Day-34/market-24-app/app/questions/components/QuestionForm.tsx
@@ -7,6 +7,9 @@ export function QuestionForm<S extends z.ZodType<any, any>>(props: FormProps<S>)
   return (
     <Form<S> {...props}>
       <LabeledTextField name="text" label="Text" placeholder="Text" />
+      <LabeledTextField name="choices.0.text" label="Choice 1 " />
+      <LabeledTextField name="choices.1.text" label="Choice 2 " />
+      <LabeledTextField name="choices.2.text" label="Choice 3 " />
     </Form>
   )
 }
