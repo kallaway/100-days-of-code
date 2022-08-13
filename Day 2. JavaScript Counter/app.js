63097c4ab28b213b89e326d4df3fb841847ac888
Salawudeen Abdultawab <opeydeen@gmail.com>
2022-08-13 13:21:42 +0100

    Day 2: JavaScript Counter

--- Day 2. JavaScript Counter/app.js
+++ Day 2. JavaScript Counter/app.js
@@ -5,22 +5,6 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
-// method 1
-/* btns.forEach(function(btn) {
-     btn.addEventListener('click', function(e) {
-          const styles = e.currentTarget.classList;
-          if (styles.contains("decrease")) {
-               count--;
-          } else if (styles.contains("increase")) {
-               count++;
-          }else {
-               count = 0;
-          }
-          value.textContent = count;
-     });
-}); */
-
-// method 2
 btns.forEach(function(btn) {
      btn.addEventListener('click', function(e) {
           const styles = e.currentTarget.classList;
