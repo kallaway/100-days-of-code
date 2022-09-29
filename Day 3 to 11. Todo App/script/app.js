e226c7043e2afe47ffe718755b01f4662c151db4
Salawudeen Abdultawab <opeydeen@gmail.com>
2022-09-29 15:20:15 +0100

    update

--- Day 3 to 11. Todo App/script/app.js
+++ Day 3 to 11. Todo App/script/app.js
@@ -4,11 +4,15 @@ const todoInputField = document.querySelector('.enterTodo');
 const todoParentContainer = document.querySelector('.todo-list');
 const todoGrandParentContainer = document.querySelector('.container-shadow');
 const emptyTodo = document.querySelector('.empty-todo');
-const todoFilter = document.querySelector('.todo-section-btns');
+const todoSections = document.querySelector('.todo-section-btns');
 const itemCount = document.querySelector('.item-count');
 let filterBtnArray = Array.from(document.querySelectorAll('.btn'));
 const clearBtn = document.querySelector('.btn-clear');
 let inputFieldArray;
+
+let todoListItems;
+let itemId = 0;
+let itemState = 'active';
 
 
 // Switch dark and light mode
@@ -16,191 +20,149 @@ themeToggle.addEventListener('click', () => {
   mainContainer.classList.toggle('darkTheme-active');
 })
 
-// main functionality
-
-let todoListItems = [];
-let itemId = 0;
-let itemState = 'active';
-itemCount.textContent = todoListItems.length;
-
 window.addEventListener('keydown', (e) => {
   if(e.key === 'Enter'){
     if(todoInputField.value !== '') {
-      itemId++;
-      let enteredTodoItem = todoInputField.value;
-
-      let todoItemInfo = {
-        id:itemId,
-        itemValue: enteredTodoItem,
-        state: itemState
-      }
-
-      todoListItems.push(todoItemInfo);
-      updateTodoHTML(todoListItems);
-      itemCount.textContent = todoListItems.length;
-      todoInputField.value = '';     
+      
+      updateTodoHtml();
+      itemCount.textContent = todoParentContainer.childElementCount;
     }
   }
 });
 
-checkboxFunction();
-refreshTodoFilter();
 
-function updateTodoHTML(todoListArray) {
-  if(todoListArray.length > 0){
-    let result = todoListArray.map((item) => {
-      return `
-        <div class="input-field todo-list-inputField" data-id=${item.id} data-state=${item.state}>
-          <div class="checkbox-container" data-id=${item.id}>
-            <img src="./images/icon-check.svg" alt="checkmark" class="checkmark" data-id=${item.id}>
-          </div>
-          <p class="text-field todoItem" data-id=${item.id}>${item.itemValue}</p>
-          <img src="./images/icon-cross.svg" alt="cancel button" class="cancel-btn" data-id=${item.id}>
-      </div>
-    `
-    }).join('');
-    emptyTodo.style.display = 'none';
-    todoParentContainer.style.display = 'block';
-    todoParentContainer.innerHTML = result;  
-    inputFieldArray = Array.from(document.querySelectorAll('.todo-list-inputField'));    
-  } else {
-    todoParentContainer.style.display = 'none';
+
+function updateTodoHtml(){
+  itemId++;
+  let enteredTodoItem = todoInputField.value;
+
+  const divElement = document.createElement('div');
+  divElement.classList.add('input-field', 'todo-list-inputField')
+  const idAttr = document.createAttribute('data-id');
+  const stateAttr = document.createAttribute('data-state');
+  idAttr.value = itemId;
+  stateAttr.value = itemState;
+  divElement.setAttributeNode(idAttr);
+  divElement.setAttributeNode(stateAttr);
+  
+  divElement.innerHTML = `
+    <div class="checkbox-container" data-id="${itemId}">
+      <img src="./images/icon-check.svg" alt="checkmark" class="checkmark" data-id="${itemId}">
+    </div>
+    <p class="text-field todoItem" data-id="${itemId}">${enteredTodoItem}</p>
+    <img src="./images/icon-cross.svg" alt="delete button" class="delete-btn" data-id="${itemId}">
+  `
+
+  const deleteBtn = divElement.querySelector('.delete-btn');
+  const checkboxes = divElement.querySelector('.checkbox-container');
+
+  checkboxes.addEventListener('click', checkboxFunction)
+  deleteBtn.addEventListener('click', deleteItem)
+
+  todoParentContainer.appendChild(divElement);
+  todoListItems = Array.from(document.querySelectorAll('.todo-list-inputField'));
+  emptyTodo.style.display = 'none';
+  todoParentContainer.style.display = 'block';
+  setDefault();
+};
+
+// checkboxFunction();
+
+function deleteItem(e){
+  let element = e.currentTarget.parentElement;
+  todoParentContainer.removeChild(element);
+  let itemNumber = todoParentContainer.childElementCount;
+  itemCount.textContent = itemNumber;
+
+  if(itemNumber === 0){
     emptyTodo.style.display = 'grid';
+    todoParentContainer.style.display = 'none';
+  } 
+}
+
+function setDefault(){
+  todoInputField.value = '';
+}
+
+function checkboxFunction(e) {
+  let targetClass = e.currentTarget.classList;
+  let parentContainer = e.currentTarget.parentElement;
+  let grandParentContainer = e.currentTarget.parentElement.parentElement;
+  let targetId = e.currentTarget.dataset.id;
+  let parentId = parentContainer.dataset.id;
+  let grandParentId = grandParentContainer.dataset.id;
+
+  
+  if(targetClass.contains('checkbox-container')){
+    parentContainer.classList.toggle('active-checkbox');
+    // console.log(targetId, parentId);
+    if(targetId == parentId){
+      changeState(parentContainer);
+    } 
+  } 
+
+  if (targetClass.contains('checkmark')) {
+    grandParentContainer.classList.toggle('active-checkbox');
+    if(targetId == grandParentId){
+      changeState(grandParentContainer);
+    }
   }
 }
 
-function checkboxFunction() {
-  todoParentContainer.addEventListener('click', (e) => {
-    let targetClass = e.target.classList;
-     let parentContainer = e.target.parentElement;
-     let grandParentContainer = e.target.parentElement.parentElement;
-     let targetId = e.target.dataset.id;
-     let parentId = parentContainer.dataset.id;
-     let grandParentId = grandParentContainer.dataset.id;
-
-    if(targetClass.contains('checkbox-container')){
-      parentContainer.classList.toggle('active-checkbox');
-      // console.log(targetId, parentId);
-      if(targetId == parentId){
-        let textValue = parentContainer.children[1];
-        textValue.classList.toggle('completed-Todo');
-        
-        for(let i = 0; i < todoListItems.length; i++){
-          let todoItemId = todoListItems[i].id;
-          if(textValue.classList.contains('completed-Todo')){
-            if(targetId == todoItemId){
-              todoListItems[i].state = 'completed';
-              inputFieldArray[i].dataset.state = 'completed';
-              return;
-            }
-            
-          } else {
-            todoListItems[i].state = 'active';
-            inputFieldArray[i].dataset.state = 'active';
-            return
-          }
-
-        }
-      }
-    } 
-
-
-    if (targetClass.contains('checkmark')) {
-      grandParentContainer.classList.toggle('active-checkbox');
-      if(targetId == grandParentId){
-        // console.log(targetId, grandParentId);
-        let textValue = grandParentContainer.children[1];
-        textValue.classList.toggle('completed-Todo');
-        for(let i = 0; i < todoListItems.length; i++){
-          let todoItemId = todoListItems[i].id;
-          if(!textValue.classList.contains('completed-Todo')){
-            if(targetId == todoItemId){
-              todoListItems[i].state = 'active';
-              inputFieldArray[i].dataset.state = 'active';
-              // updateTodoHTML(todoListItems);
-              return;
-            }
-          } 
-        }
-      }
-    }
-
-    if(targetClass.contains('cancel-btn')){
-      for(let i = 0; i < todoListItems.length; i++){
-        if(targetId == parentId){
-          parentContainer.classList.add('hide-input-field');
-          todoListItems.splice(i, 1);
-          // updateTodoHTML(todoListItems);
-          itemCount.textContent = todoListItems.length;
-          return;
-        }
-      }
-    }
-  })
+function changeState(container){
+  let textValue = container.children[1];
+  textValue.classList.toggle('completed-Todo');
+  if(textValue.classList.contains('completed-Todo')){
+    container.dataset.state = 'completed';
+    // console.log(container);
+  } else {
+    container.dataset.state = 'active';
+    // console.log(container);
+  }
 }
 
-function refreshTodoFilter() {
-  todoFilter.addEventListener('click', (e) => {
+
+  todoSections.addEventListener('click', (e) => {
     let filterBtnClass = e.target.classList;
-    let filterState = e.target.dataset.filter;
+    // let filterState = e.target.dataset.filter;
 
     filterBtnArray.map((e) => {
       if(e.classList.contains('active-section')){
         e.classList.remove('active-section');
       }
     })
+
     filterBtnClass.add('active-section');
 
-    if(filterBtnClass.contains('btn-complete')){
-      for(let i = 0; i < inputFieldArray.length; i++){
-        let inputFieldClasses = inputFieldArray[i].classList;
-        let inputFieldState = inputFieldArray[i].dataset.state;
-        // inputFieldClasses.remove('hide-input-field');
-        if(inputFieldState !== 'completed'){
-          inputFieldClasses.add('hide-input-field');
-        } else {
-          inputFieldClasses.remove('hide-input-field');
-        }
-      }
-    }
-
     if(filterBtnClass.contains('btn-active')){
-      for(let i = 0; i < inputFieldArray.length; i++){
-        let inputFieldClasses = inputFieldArray[i].classList;
-        let inputFieldState = inputFieldArray[i].dataset.state;
-        inputFieldClasses.remove('hide-input-field');
-        if(inputFieldState !== 'active'){
-          inputFieldClasses.add('hide-input-field');
-        } else {
-          inputFieldClasses.remove('hide-input-field');
+      let activeFilter = todoListItems.filter((item) => {
+        if(item.dataset.state === 'active'){
+          return item
         }
-      }
-    }
+      })
 
-    if(filterBtnClass.contains('btn-all')){
-      for(let i = 0; i < inputFieldArray.length; i++){
-        let inputFieldClasses = inputFieldArray[i].classList;
-        inputFieldClasses.remove('hide-input-field');
-      }
-    }
+      // console.log(activeFilter);
+      // console.log(todoListItems);
 
-  //   let todoSections = todoListItems.filter((item) => {
-  //     if(filterState == item.state){
-  //       return item;
-  //     }
-  //   });
-  
-  //   if(filterState == 'all'){
-  //     updateTodoHTML(todoListItems);
-  //   } else {
-  //     updateTodoHTML(todoSections);
-  //   }
+      // console.log(...activeFilter);
+
+      const result = (arrays) => {
+        for(let i = 0; i < todoListItems.length; i++){
+          return arrays[i];
+        }
+        
+      }
+
+      let answer = result(activeFilter);
+      console.log(answer)
+
+      // todoParentContainer.innerHTML = `$`
+      
+      
+    }
   })
-}
 
+  
 
-
-
-
-
-
+  
+  
--- Day 3 to 11. Todo App/script/blaab.js
+++ Day 3 to 11. Todo App/script/blaab.js
@@ -1,181 +1,206 @@
-todoFilter.addEventListener('click', (e) => {
-  let filterBtnClass = e.target.classList;
-  if(filterBtnClass.contains('btn-complete')){
-    const completedItems = todoListItems.filter((item) => {
-      // console.log(item.status);
-      return completedItems
-    })
-    console.log(completedItems);
-  }
+const mainContainer = document.querySelector('.main');
+const themeToggle = document.querySelector('.theme-toggle');
+const todoInputField = document.querySelector('.enterTodo');
+const todoParentContainer = document.querySelector('.todo-list');
+const todoGrandParentContainer = document.querySelector('.container-shadow');
+const emptyTodo = document.querySelector('.empty-todo');
+const todoFilter = document.querySelector('.todo-section-btns');
+const itemCount = document.querySelector('.item-count');
+let filterBtnArray = Array.from(document.querySelectorAll('.btn'));
+const clearBtn = document.querySelector('.btn-clear');
+let inputFieldArray;
+
+
+// Switch dark and light mode
+themeToggle.addEventListener('click', () => {
+  mainContainer.classList.toggle('darkTheme-active');
 })
 
-if(textValue.classList.contains('completed-Todo')){
-          for(let i = 0; i < todoListItems.length; i++){
-            let todoItemId = todoListItems[i].id;
-            if(targetId = todoItemId){
+// main functionality
+
+let todoListItems = [];
+let itemId = 0;
+let itemState = 'active';
+itemCount.textContent = todoListItems.length;
+
+window.addEventListener('keydown', (e) => {
+  if(e.key === 'Enter'){
+    if(todoInputField.value !== '') {
+      itemId++;
+      let enteredTodoItem = todoInputField.value;
+
+      let todoItemInfo = {
+        id:itemId,
+        itemValue: enteredTodoItem,
+        state: itemState
+      }
+
+      todoListItems.push(todoItemInfo);
+      updateTodoHTML(todoListItems);
+      itemCount.textContent = todoListItems.length;
+      todoInputField.value = '';     
+    }
+  }
+});
+
+checkboxFunction();
+refreshTodoFilter();
+
+function updateTodoHTML(todoListArray) {
+  if(todoListArray.length > 0){
+    let result = todoListArray.map((item) => {
+      return `
+        <div class="input-field todo-list-inputField" data-id=${item.id} data-state=${item.state}>
+          <div class="checkbox-container" data-id=${item.id}>
+            <img src="./images/icon-check.svg" alt="checkmark" class="checkmark" data-id=${item.id}>
+          </div>
+          <p class="text-field todoItem" data-id=${item.id}>${item.itemValue}</p>
+          <img src="./images/icon-cross.svg" alt="cancel button" class="cancel-btn" data-id=${item.id}>
+      </div>
+    `
+    }).join('');
+    emptyTodo.style.display = 'none';
+    todoParentContainer.style.display = 'block';
+    todoParentContainer.innerHTML = result;  
+    inputFieldArray = Array.from(document.querySelectorAll('.todo-list-inputField'));    
+  } else {
+    todoParentContainer.style.display = 'none';
+    emptyTodo.style.display = 'grid';
+  }
+}
+
+function checkboxFunction() {
+  todoParentContainer.addEventListener('click', (e) => {
+    let targetClass = e.target.classList;
+     let parentContainer = e.target.parentElement;
+     let grandParentContainer = e.target.parentElement.parentElement;
+     let targetId = e.target.dataset.id;
+     let parentId = parentContainer.dataset.id;
+     let grandParentId = grandParentContainer.dataset.id;
+
+    if(targetClass.contains('checkbox-container')){
+      parentContainer.classList.toggle('active-checkbox');
+      // console.log(targetId, parentId);
+      if(targetId == parentId){
+        let textValue = parentContainer.children[1];
+        textValue.classList.toggle('completed-Todo');
+        
+        for(let i = 0; i < todoListItems.length; i++){
+          let todoItemId = todoListItems[i].id;
+          if(textValue.classList.contains('completed-Todo')){
+            if(targetId == todoItemId){
               todoListItems[i].state = 'completed';
+              inputFieldArray[i].dataset.state = 'completed';
               return;
             }
-          }
-        }
-
-
-        const mainContainer = document.querySelector('.main');
-        const themeToggle = document.querySelector('.theme-toggle');
-        // const todoListContainer = document.querySelector('.todo-list');
-        let todoInputField = document.querySelector('.enterTodo');
-        let todoParentContainer = document.querySelector('.todo-list');
-        let todoGrandParentContainer = document.querySelector('.container-shadow');
-        let emptyTodo = document.querySelector('.empty-todo');
-        let todoFilter = document.querySelector('.info-panel');
-        // let itemState = document.querySelector('.todo-list-inputField').dataset.state;
-        // console.log(itemState);
-        
-        
-        // Switch dark and light mode
-        themeToggle.addEventListener('click', () => {
-          mainContainer.classList.toggle('darkTheme-active');
-        })
-        
-        // main functionality
-        
-        let todoListItems = [];
-        let itemId = 0;
-        let itemState = 'active';
-        
-        window.addEventListener('keydown', (e) => {
-          if(e.key === 'Enter'){
-            if(todoInputField.value !== '') {
-              let enteredTodoItem = todoInputField.value;
-        
-        
-              let todoItemInfo = {
-                id:itemId,
-                itemValue: enteredTodoItem,
-                state: itemState
-              }
-              todoListItems.push(todoItemInfo);
-              itemId++;
-        
-              if(todoListItems.length > 0) {
-                updateTodoHTML();
-                todoInputField.value = '';
-                console.log(todoListItems);
-              } 
-            }
-          }
-        })
-        
-        checkboxFunction();
-        
-        function updateTodoHTML() {
-          if(todoListItems.length > 0){
-            let result = todoListItems.map((item) => {
-              return `
-                <div class="input-field todo-list-inputField" data-id=${item.id} data-state=${item.state}>
-                  <div class="checkbox-container" data-id=${item.id}>
-                    <img src="./images/icon-check.svg" alt="checkmark" class="checkmark">
-                  </div>
-                  <p class="text-field todoItem" data-id=${item.id}>${item.itemValue}</p>
-                  <img src="./images/icon-cross.svg" alt="cancel button" class="cancel-btn" data-id=${item.id}>
-              </div>
-            `
-            })
-            emptyTodo.style.display = 'none';
-            todoParentContainer.style.display = 'block';
-            todoParentContainer.innerHTML = result.join('');
+            
           } else {
-            todoParentContainer.style.display = 'none';
-            emptyTodo.style.display = 'grid';
+            todoListItems[i].state = 'active';
+            inputFieldArray[i].dataset.state = 'active';
+            return
           }
+
         }
-        
-        
-        function checkboxFunction() {
-          todoParentContainer.addEventListener('click', (e) => {
-            let targetClass = e.target.classList;
-             let parentContainer = e.target.parentElement;
-             let grandParentContainer = e.target.parentElement.parentElement;
-             let targetId = e.target.dataset.id;
-             let parentId = parentContainer.dataset.id;
-             let grandParentId = grandParentContainer.dataset.id;
-            //  console.log({targetClass}, {parentContainer}, {grandParentContainer},
-            //   {targetId}, {parentId}, {grandParentId});
-            console.log({targetId}, {parentId});
-            if(targetClass.contains('checkbox-container')){
-              parentContainer.classList.toggle('active-checkbox');
-              if(targetId = parentId){
-                // console.dir(parentContainer);
-                let textValue = parentContainer.children[targetId];
-                textValue.classList.toggle('completed-Todo'); 
-        
-                if(textValue.classList.contains('completed-Todo')){
-                  for(let i = 0; i < todoListItems.length; i++){
-                    let todoItemId = todoListItems[i].id;
-                    if(targetId = todoItemId){
-                      let selected = (todoListItems[todoItemId]);
-                      console.log({selected});
-                      console.log({targetId}, {todoItemId});
-                      return;
-                    }
-                  }
-                } 
-              }
-            } 
-            else if (targetClass.contains('checkmark')) {
-              grandParentContainer.classList.toggle('active-checkbox');
-              if(targetId = grandParentId){
-                let textValue = grandParentContainer.children[1];
-                textValue.classList.toggle('completed-Todo');
-        
-                // if(!textValue.classList.contains('completed-Todo')){
-                //   for(let i = 0; i < todoListItems.length; i++){
-                //     todoListItems[i].state = 'active';
-                //     console.log(todoListItems);
-                //     return;
-                //   }
-                // }
-              }
-            }
-        
-            if(targetClass.contains('cancel-btn')){
-              // console.log(targetId, parentId);
-              for(let i = 0; i < todoListItems.length; i++){
-                if(targetId = parentId){
-                  todoListItems.splice(i, 1);
-                  console.log(todoListItems);
-                  updateTodoHTML();
-                  return;
-                }
-              }
-            }
-          })
-        }
-        
-        // todoFilter.addEventListener('click', (e) => {
-        //   let filterBtnClass = e.target.classList;
-        //   if(filterBtnClass.contains('btn-complete')){
-        //     const completedItems = todoListItems.filter((item) => {
-        //       // console.log(item.status);
-        //       return completedItems
-        //     })
-        //     console.log(completedItems);
-        //   }
-        // })
+      }
+    } 
 
 
-        clearBtn.addEventListener('click', () => {
-          for(let i = 0; i < inputFieldArray.length; i++){
-            let inputFieldClasses = inputFieldArray[i].classList;
-            let inputFieldState = inputFieldArray[i].dataset.state;
-            if(inputFieldState == 'completed'){
-              inputFieldClasses.add('hide-input-field');
+    if (targetClass.contains('checkmark')) {
+      grandParentContainer.classList.toggle('active-checkbox');
+      if(targetId == grandParentId){
+        // console.log(targetId, grandParentId);
+        let textValue = grandParentContainer.children[1];
+        textValue.classList.toggle('completed-Todo');
+        for(let i = 0; i < todoListItems.length; i++){
+          let todoItemId = todoListItems[i].id;
+          if(!textValue.classList.contains('completed-Todo')){
+            if(targetId == todoItemId){
+              todoListItems[i].state = 'active';
+              inputFieldArray[i].dataset.state = 'active';
               // updateTodoHTML(todoListItems);
+              return;
             }
-          }
-          
-        })
-        
-        
-        
-        
+          } 
+        }
+      }
+    }
+
+    if(targetClass.contains('cancel-btn')){
+      for(let i = 0; i < todoListItems.length; i++){
+        if(targetId == parentId){
+          parentContainer.classList.add('hide-input-field');
+          todoListItems.splice(i, 1);
+          // updateTodoHTML(todoListItems);
+          itemCount.textContent = todoListItems.length;
+          return;
+        }
+      }
+    }
+  })
+}
+
+function refreshTodoFilter() {
+  todoFilter.addEventListener('click', (e) => {
+    let filterBtnClass = e.target.classList;
+    let filterState = e.target.dataset.filter;
+
+    filterBtnArray.map((e) => {
+      if(e.classList.contains('active-section')){
+        e.classList.remove('active-section');
+      }
+    })
+    filterBtnClass.add('active-section');
+
+    if(filterBtnClass.contains('btn-complete')){
+      for(let i = 0; i < inputFieldArray.length; i++){
+        let inputFieldClasses = inputFieldArray[i].classList;
+        let inputFieldState = inputFieldArray[i].dataset.state;
+        // inputFieldClasses.remove('hide-input-field');
+        if(inputFieldState !== 'completed'){
+          inputFieldClasses.add('hide-input-field');
+        } else {
+          inputFieldClasses.remove('hide-input-field');
+        }
+      }
+    }
+
+    if(filterBtnClass.contains('btn-active')){
+      for(let i = 0; i < inputFieldArray.length; i++){
+        let inputFieldClasses = inputFieldArray[i].classList;
+        let inputFieldState = inputFieldArray[i].dataset.state;
+        inputFieldClasses.remove('hide-input-field');
+        if(inputFieldState !== 'active'){
+          inputFieldClasses.add('hide-input-field');
+        } else {
+          inputFieldClasses.remove('hide-input-field');
+        }
+      }
+    }
+
+    if(filterBtnClass.contains('btn-all')){
+      for(let i = 0; i < inputFieldArray.length; i++){
+        let inputFieldClasses = inputFieldArray[i].classList;
+        inputFieldClasses.remove('hide-input-field');
+      }
+    }
+
+  //   let todoSections = todoListItems.filter((item) => {
+  //     if(filterState == item.state){
+  //       return item;
+  //     }
+  //   });
+  
+  //   if(filterState == 'all'){
+  //     updateTodoHTML(todoListItems);
+  //   } else {
+  //     updateTodoHTML(todoSections);
+  //   }
+  })
+}
+
+
+
+
+
+
+
