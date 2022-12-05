5cab140ca2ca988a30d1a6177e46d476715a3b56
Abdul nasir V <108569716+AbdulNasir-NuZ@users.noreply.github.com>
2022-12-05 20:12:56 +0530

    Add files via upload

---
+++ Day_7-exercisepython/main.py
@@ -0,0 +1,14 @@
+# Declaratiing 2 variables.
+
+a = 10
+b = 6
+
+# Printing operations on the variables.
+
+print("Adding variables,",a + b)
+print("Subtracting variables,",a - b)
+print("Multiplying variables,",a * b)
+print("Dividing variables,",a / b)
+print("Remainder,",a % b)
+print("Exponential,",a ** b)
+print("Without decimals,",a // b)
