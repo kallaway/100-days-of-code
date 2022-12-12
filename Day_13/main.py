da50035fe3a18b507f627369a4cd231363251013
Abdul nasir V <108569716+AbdulNasir-NuZ@users.noreply.github.com>
2022-12-12 11:06:54 +0530

    Add files via upload

---
+++ Day_13/main.py
@@ -0,0 +1,56 @@
+# Strings are immutable
+a = "!!!Harry!! !!!!!!!!! Harry"
+print(len(a))
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("Harry", "John"))
+print(a.split(" "))
+blogHeading = "introduction tO jS"
+print(blogHeading.capitalize())
+
+str1 = "Welcome to the Console!!!"
+print(len(str1))
+print(len(str1.center(50)))
+print(a.count("Harry"))
+
+str1 = "Welcome to the Console !!!"
+print(str1.endswith("!!!"))
+
+str1 = "Welcome to the Console !!!"
+print(str1.endswith("to", 4, 10))
+
+str1 = "He's name is Dan. He is an honest man."
+print(str1.find("ishh"))
+# print(str1.index("ishh"))
+
+str1 = "WelcomeToTheConsole"
+print(str1.isalnum())
+str1 = "Welcome"
+print(str1.isalpha())
+
+str1 = "hello world"
+print(str1.islower())
+
+str1 = "We wish you a Merry Christmas\n"
+print(str1.isprintable())
+str1 = "         "       #using Spacebar
+print(str1.isspace())
+str2 = "  "       #using Tab
+print(str2.isspace())
+
+str1 = "World Health Organization" 
+print(str1.istitle())
+
+str2 = "To kill a Mocking bird"
+print(str2.istitle())
+
+str1 = "Python is a Interpreted Language" 
+print(str1.startswith("Python"))
+
+str1 = "Python is a Interpreted Language" 
+print(str1.swapcase())
+
+str1 = "His name is Dan. Dan is an honest man."
+print(str1.title())
---
+++ Day_14/01 myif.py
@@ -0,0 +1,15 @@
+a = int(input("Enter your age: "))
+print("Your age is:", a)
+# Conditional operators 
+# >, <, >=, <=, ==, !=
+# print(a>18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+if(a>18):
+  print("You can drive")
+  print("Yes")
+else:
+  print("You cannot drive")
+  print("No")
+  print("Yay!")
