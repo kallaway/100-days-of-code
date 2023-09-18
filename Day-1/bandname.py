f9a5e0061f1038289413c36d4f2415201228d1c1
absenth <1115138+absenth@users.noreply.github.com>
2023-09-18 10:14:08 -0500

    uploading Day1 project, bandname.py

---
+++ Day-1/bandname.py
@@ -0,0 +1,31 @@
+import requests
+
+
+def main():
+    print('Do you want to use a random band name? Yes/No')
+    selection = input()
+    if selection in ('y', 'yes'):
+        bandname = []
+        for w in range(2):
+            word = getword()
+            bandname.append(word)
+        print(f'Your band name should be {bandname[0]} {bandname[1]}')
+    else:
+        bandname = coursename()
+        print(f'Your band name should be {bandname}')
+
+
+def getword():
+    return requests.get('https://random-word-api.herokuapp.com/word').json()[0]
+
+
+def coursename():
+    print('What city did you grow up in?')
+    city = input()
+    print('What was the name of your favorite pet?')
+    pet = input()
+    return f'{city} {pet}'
+
+
+if __name__ == '__main__':
+    main()
