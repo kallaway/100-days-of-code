ae13c79a9e6212490f445f96606004f4ce4ba4bc
absenth <1115138+absenth@users.noreply.github.com>
2023-09-20 18:14:55 -0500

    ascii art for great good

--- Day-3/treasureisland.py
+++ Day-3/treasureisland.py
@@ -1,4 +1,6 @@
 import subprocess, os
+from drawing import asciiart
+import time
 
 
 def main():
@@ -11,32 +13,53 @@ def main():
 
 
 def crossroads():
+    gs = 0
+    asciiart(gs)
     direction = input('You are at cross road.  Where do you want to go?  Type "left" or "right" ')
     if direction.lower() in ("r", "right"):
+        gs = 3
+        asciiart(gs)
         print('You fell into a hole.  Game Over.')
         quit()
     else:
+        time.sleep(3)
+        clearscreen()
         return True
 
 
 def lake():
+    gs = 1
+    asciiart(gs)
     cross = input('You have come to a lake.  There is an island in the middle of the lake.  Type "wait" to wait for a boat.  Type "swim" to swim across. ')
     if cross.lower() in ('s', 'swim'):
+        gs = 4
+        asciiart(gs)
         print('You get attacked by an angry trout.  Game Over.')
         quit()
     else:
+        time.sleep(3)
+        clearscreen()
         return True
 
 
 def house():
+    gs = 2
+    asciiart(gs)
     door = input('You arrive at the island unharmed.  There is a house with 3 doors.  One red, one yellow, and one blue.  Which color do you choose? ')
     if door.lower() in ('r', 'red'):
+        clearscreen()
+        gs = 5
+        asciiart(gs)
         print('It is a room full of fire. Game Over.')
         quit()
     elif door.lower() in ('b', 'blue'):
+        gs = 6
+        clearscreen()
+        asciiart(gs)
         print('You enter a room full of beasts.  Game Over')
         quit()
     elif door.lower() in ('y', 'yellow'):
+        clearscreen()
         print('You have found the treasure.  You Win!')
         return True
     else:
@@ -46,7 +69,7 @@ def house():
 
 def win():
     print('''
-    ******************************************************************************* 
+    *******************************************************************************
               |                   |                  |                     |
      _________|________________.=""_;=.______________|_____________________|_______
     |                   |  ,-"_,=""     `"=.|                  |
@@ -72,14 +95,15 @@ def win():
 def welcome():
     print('Welcome to Treasure Island.')
     print('Your mission is to find the treasure.')
+    time.sleep(3)
     return
 
 
 def clearscreen():
-    if os.name is 'posix':
+    if os.name == 'posix':
         os.system('clear')
         return True
-    elif os.name is 'nt':
+    elif os.name == 'nt':
         os.system('cls')
         return True
     else:
