55b6409c74d0711023733473aafb716343f3a5c9
absenth <1115138+absenth@users.noreply.github.com>
2023-10-09 13:32:21 -0500

    adding lizard spock to rock paper scissors

--- Day-4/rps.py
+++ Day-4/rps.py
@@ -5,8 +5,8 @@ import random
 def main():
     clearscreen()
     print('Welcome to Rock Paper Scissors')
-    playerchoice = input('Please select one of the following: "rock", "Paper", or "Scissors" ').lower()
-    computerchoice = random.choice(['r','s','p'])
+    playerchoice = input('Please select one of the following: "rock", "Paper", "Scissors", "Lizard", or "Spock" ').lower()
+    computerchoice = random.choice(['r','s','p','l','k'])
     result = buildresult(playerchoice, computerchoice)
     printout, wld = gamestate(result)
     print(f'{printout} - as a result you {wld}')
@@ -18,8 +18,10 @@ def main():
 
 
 def buildresult(playerchoice, computerchoice):
-    if playerchoice in ('rock', 'r') or playerchoice in ('paper', 'p') or playerchoice in ('scissors', 's'):
+    if playerchoice in ('rock') or playerchoice in ('paper') or playerchoice in ('scissors') or playerchoice in ('lizard'):
         return playerchoice[0] + computerchoice
+    elif playerchoice in ('spock'):
+        return 'k' + computerchoice
     else:
         print(f'You picked {playerchoice} which is not rock, paper or scissors. You fail for not following directions')
         quit()
@@ -31,30 +33,77 @@ def gamestate(gs):
             'out': 'We both played rock.',
             'winlosetie': 'draw'},
         'rp': {
-            'out': 'You played rock but I played paper.',
+            'out': 'My paper covers your rock.',
             'winlosetie': 'lose'},
         'rs': {
-            'out': 'You played rock and I played scisors.',
+            'out': 'Your rock smashes my scissors',
+            'winlosetie': 'win'},
+        'rk': {
+            'out': 'My spock vaporizes your rock',
+            'winlosetie': 'lose'},
+        'rl': {
+            'out': 'Your rock crushes my lizard',
             'winlosetie': 'win'},
         'pr': {
-            'out': 'You played paper and I played rock.',
+            'out': 'Your paper covers my rock',
             'winlosetie': 'win'},
         'pp': {
             'out': 'We both played paper',
             'winlosetie': 'draw'},
         'ps': {
-            'out': 'You played paper but I played scissors.',
+            'out': 'My scissors cut your paper',
+            'winlosetie': 'lose'},
+        'pk': {
+            'out': 'Your paper disproves my spock',
+            'winlosetie': 'win'},
+        'pl': {
+            'out': 'My lizard eats your paper',
             'winlosetie': 'lose'},
         'sr': {
-            'out': 'You played scissors, but I played rock',
+            'out': 'My rock smashes your scissors',
             'winlosetie': 'lose'},
         'sp': {
-            'out': 'You played scissors, and I played paper.',
+            'out': 'Your scissors cut my paper',
             'winlosetie': 'win'},
         'ss': {
             'out': 'We both played scissors',
-            'winlosetie': 'draw'
-            }
+            'winlosetie': 'draw'},
+        'sk': {
+            'out': 'My spock smashes your scissors',
+            'winlosetie': 'lose'},
+        'sl': {
+            'out': 'Your scissors decapitate my lizard',
+            'winlosetie': 'win'},
+        'kr': {
+            'out': 'Your spock vaporizes my rock',
+            'winlosetie': 'win'},
+        'kp': {
+            'out': 'My paper disproves your spock',
+            'winlosetie': 'lose'},
+        'ks': {
+            'out': 'Your spock smashes my scissors',
+            'winlosetie': 'win'},
+        'kk': {
+            'out': 'We both played spock',
+            'winlosetie': 'draw'},
+        'kl': {
+            'out': 'My lizard poisons your spock',
+            'winlosetie': 'lose'},
+        'lr': {
+            'out': 'My rock crushes your lizard',
+            'winlosetie': 'lose'},
+        'lp': {
+            'out': 'Your lizard eats my paper',
+            'winlosetie': 'win'},
+        'ls': {
+            'out': 'My scissors decapitate your lizard',
+            'winlosetie': 'lose'},
+        'lk': {
+            'out': 'Your lizard poisons my spock',
+            'winlosetie': 'win'},
+        'll': {
+            'out': 'We both played lizard',
+            'winlosetie': 'draw'},
     }
 
     result = config[gs]
