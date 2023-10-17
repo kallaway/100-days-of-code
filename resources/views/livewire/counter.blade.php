9d455a29cfce760b96b256aa8e826a2135eacb52
mdbilal <mdbilalofficial@gmail.com>
2023-10-18 03:47:19 +0500

    counter example done

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,8 @@
+<div>
+    {{-- Care about people's approval and you will be their prisoner. --}}
+    <h1>{{ $count }}</h1>
+
+    <button wire:click="increment">+</button>
+
+    <button wire:click="decrement">-</button>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Counter;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/counter', Counter::class);
